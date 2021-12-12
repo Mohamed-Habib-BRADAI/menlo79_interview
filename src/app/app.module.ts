e94986f9bca25742e198b11effece0990cfb40a3
Bradai <bradai@Bradais-MacBook-Air.local>
2021-12-12 18:41:44 +0100

    feat / add routing and project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideMenuComponent } from './layout/side-menu/side-menu.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, SideMenuComponent, HeaderComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
   providers: [],
+  exports: [SideMenuComponent, HeaderComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'service',
+        loadChildren: () => import('../components/service/service.module').then((m) => m.ServiceModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule {}
